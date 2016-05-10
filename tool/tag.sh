2eb143f017041a06031aeb3e03c32f4fda3fe911
Jake Yang <yanghuanjie@cndoppler.cn>
2016-05-10 14:12:19 +0800

    修复: 获取版本问题

--- tool/tag.sh
+++ tool/tag.sh
@@ -8,19 +8,22 @@ version=1.0.0
 
 for tag in `git tag`; do
 	tmp=`echo ${tag} | awk -F"." '{print $1}'`
-	if [ "${tmp}" -gt "${MAJOR}" ]; then
-		MAJOR=${tmp}
+	if [ "${tmp}" -lt "${MAJOR}" ]; then
+		continue
 	fi
+	MAJOR=${tmp}
 
 	tmp=`echo ${tag} | awk -F"." '{print $2}'`
-	if [ "${tmp}" -gt "${MINOR}" ]; then
-		MINOR=${tmp}
+	if [ "${tmp}" -lt "${MINOR}" ]; then
+		continue
 	fi
+	MINOR=${tmp}
 	
 	tmp=`echo ${tag} | awk -F"." '{print $3}'`
 	if [ "${tmp}" -gt "${MICRO}" ]; then
-		MICRO=${tmp}
+		continue
 	fi
+	MICRO=${tmp}
 done
 
 echo ${MAJOR}.${MINOR}.${MICRO}
