5b9f34d7fac03b6377bf87e87a3e61423ce9ab50
Jake Yang <yanghuanjie@cndoppler.cn>
2016-05-25 13:35:48 +0800

    修复: 获取版本号问题

--- tool/tag.sh
+++ tool/tag.sh
@@ -1,30 +1,7 @@
 #!/bin/sh
 
-MAJOR=1
-MINOR=0
-MICRO=0
+version=`git lg | grep "tag:" | head -n 1 | cut -d":" -f2 | cut -d")" -f1 | cut -d"," -f1 | cut -d" " -f2`
 
-version=1.0.0
 
-for tag in `git tag`; do
-	tmp=`echo ${tag} | awk -F"." '{print $1}'`
-	if [ "${tmp}" -lt "${MAJOR}" ]; then
-		continue
-	fi
-	MAJOR=${tmp}
-
-	tmp=`echo ${tag} | awk -F"." '{print $2}'`
-	if [ "${tmp}" -lt "${MINOR}" ]; then
-		continue
-	fi
-	MINOR=${tmp}
-	
-	tmp=`echo ${tag} | awk -F"." '{print $3}'`
-	if [ "${tmp}" -gt "${MICRO}" ]; then
-		continue
-	fi
-	MICRO=${tmp}
-done
-
-echo ${MAJOR}.${MINOR}.${MICRO}
+echo ${version}
 
