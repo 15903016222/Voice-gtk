1b55936846887dee0da939c7a9af15532e1343c8
Rongzuo Xu <xurongzuo@cndoppler.cn>
2016-09-17 22:58:54 -0700

    修正.

--- src/callback.c
+++ src/callback.c
@@ -1609,8 +1609,10 @@ void b3_fun0(gpointer pt)
 			}
 			break;
 		case 9 :
-			g_debug ("software update\n");
-			software_update();
+            if(p->pos1[9] == 4) {
+			    //g_debug ("software update\n");
+			    software_update();
+            }
 			break;
 		default:break;
 	}
